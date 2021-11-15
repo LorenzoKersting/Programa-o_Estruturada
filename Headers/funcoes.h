ec4fd26ad82c492df3933a66962a839f64592e32
LorenzoKersting <53866766+LorenzoKersting@users.noreply.github.com>
2021-11-15 19:29:09 -0300

    Start project - Previdência e Teto de gastos

---
+++ Headers/funcoes.h
@@ -0,0 +1,9 @@
+#ifndef FUNCOES_H_INCLUDED
+#define FUNCOES_H_INCLUDED
+
+
+
+void CustoAtualPrevidencia();
+
+
+#endif /* FUNCOES_H_INCLUDED */
---
+++ Sources/main.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "lib.h"
+
+int main(){
+	
+	CustoAtualPrevidencia();
+	
+}
