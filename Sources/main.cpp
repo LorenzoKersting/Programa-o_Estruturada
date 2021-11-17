9178c5daa21ffe10c4dd03216c9a1cbe6e863a79
LorenzoKersting <53866766+LorenzoKersting@users.noreply.github.com>
2021-11-16 21:19:09 -0300

    Simple test

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,10 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "lib.h"
+  
+
+
+void CustoAtualPrevidencia(){
+	float custoAtualPrevi;
+	char confirma;
+	
+	scanf("%f", &custoAtualPrevi);
+	
+	printf("\n Valor atual do custo com previdencia eh de: %.2f", custoAtualPrevi);
+	
+}
+
+float QuantosContemplados(float salarioAtual, float custoAtualPrevi){
+	
+		float Contemplados = custoAtualPrevi / salarioAtual;
+		
+		printf("Estes são os habitantes contemplados com a previdência: %.2f", Contemplados);
+	
+}
 
 int main(){
 	
+	float salarioAtual;
+	
 	CustoAtualPrevidencia();
 	
+	QuantosContemplados(float salarioAtual, float custoAtualPrevi);
+
 }
